df29f8a5f4b633d07924a2dfba5a3676a7fecddd
davidb990 <90688786+davidb990@users.noreply.github.com>
2022-08-02 21:58:04 +0100

    Bug fixes

--- SoE.c
+++ SoE.c
@@ -32,16 +32,14 @@ void SoE(int runs) {
 
 int main(){
     int msg_count = 1;
-    while (true) {
-        unsigned long int elapsed;
-        struct timeval t0, t1, dt;
-        gettimeofday(&t0, NULL)
+    while (1) {
+        struct timeval t0, t1;
+        gettimeofday(&t0, NULL);
         SoE(SOE_LOOPS);
-        gettimeofday(&t1, NULL)
-        timeval_subtract(&dt, &t1, &t0);
-        elapsed = (dt.tv_sec*1e6 + dt.tv_usec);
-        printf("PI_1_B_MEANTIME_US:%i:MSG_COUNT:%i:\n\r", int elapsed/SOE_LOOPS, msg_count)
+        gettimeofday(&t1, NULL);
+        long seconds = (t1.tv_sec - t0.tv_sec);
+        long micros = ((seconds * 1e6) + t1.tv_usec) - (t0.tv_usec);
+        printf("PI_1_B_MEANTIME_US:%i:MSG_COUNT:%i:\n\r", int elapsed/SOE_LOOPS, msg_count);
         msg_count += 1;
-        sleep_ms(500)
     }
 }
